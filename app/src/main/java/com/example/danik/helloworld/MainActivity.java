ce741c9753851b048cf59b76264261fa44c3c3fc
Danik Fedirko <danylo.fedirko@gmail.com>
2018-09-13 11:10:06 +0300

    inputfield color fix

--- app/src/main/java/com/example/danik/helloworld/MainActivity.java
+++ app/src/main/java/com/example/danik/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.danik.helloworld;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            System.out.println("Hello World");
             inputName = findViewById(R.id.inputName);
             buttonClear = findViewById(R.id.buttonClear);
             buttonHi = findViewById(R.id.buttonHi);
@@ -27,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
             inputName.getText().clear();
         }
 
+        @SuppressLint("StringFormatInvalid")
         public void showMessage(View view) {
-            myName.setText("Hello" + " " + inputName.getText().toString());
+            myName.setText(getString(R.string.hello) + ' ' + inputName.getText());
             inputName.getText().clear();
         }
 }
