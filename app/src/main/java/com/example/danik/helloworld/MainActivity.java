8274acbb0e6d33f0ea24fed61e7ced50c35b00f7
Danik Fedirko <danylo.fedirko@gmail.com>
2018-09-13 10:45:46 +0300

    lab2

--- app/src/main/java/com/example/danik/helloworld/MainActivity.java
+++ app/src/main/java/com/example/danik/helloworld/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.danik.helloworld;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+        Button buttonHi, buttonClear;
+        EditText inputName;
+        TextView myName;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
-        System.out.println("Hello World");
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            System.out.println("Hello World");
+            inputName = findViewById(R.id.inputName);
+            buttonClear = findViewById(R.id.buttonClear);
+            buttonHi = findViewById(R.id.buttonHi);
+            myName = findViewById(R.id.myname);
+        }
+
+        public void clearMessage(View view) {
+            inputName.getText().clear();
+        }
+
+        public void showMessage(View view) {
+            myName.setText("Hello" + " " + inputName.getText().toString());
+            inputName.getText().clear();
+        }
 }
